b1415c77590799aa28ec262bad009df8474aecbb
João Pedro <joni_silvaco@hotmail.com>
2014-08-28 21:36:16 +0100

    Finished the prompt questions to the static mode

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ prompt.delimiter = "";
 prompt.start();
 
 prompt.get([{
-      name: "mode",
-      description: "What mode do you want to use? Static (1) or dynamic (2)?",
-      message: "This is a required field and you should only use numbers (1 & 2) here!",
-      type: "number",
-      required: true,
-      conform: function(mode) {
-        return mode >= 1 && mode <= 2;
+        name: "mode",
+        description: "What mode do you want to use? Static (1) or dynamic (2)?",
+        message: "This is a required field and you should only use numbers (1 & 2) here!",
+        type: "number",
+        required: true,
+        conform: function(mode) {
+          return mode >= 1 && mode <= 2;
       }}],
 function (err, results) {
   if (results.mode == 1) {
@@ -25,14 +25,27 @@ function (err, results) {
 
 function staticMode() {
   prompt.get([{
-        name: "total",
-        description: "Total?",
-        message: "This is a required field and you should use a number greater than 0!",
-        type: "number",
-        required: true,
-        conform: function(total) {
-          return total >= 1;
-        }}],
+          name: "total",
+          description: "How much time do you want to count?",
+          message: "This is a required field and you should use a number greater than 0!",
+          type: "number",
+          required: true,
+          conform: function(total) {
+            return total >= 1;
+          }
+        },
+        {
+          name: "between",
+          description: "How much time do you want between warnings?",
+          message: "This is a required field and you should use a number greater than 0 and less than the total time!", // Only valid if total % between isn't greater than 0
+          type: "number",
+          required: true,
+          conform: function(between) {
+            var total = prompt.history("total").value;
+            console.log(total % between == 0);
+            return between < total && total % between == 0;
+          }
+        }],
   function (err, results) {
     console.log(results);
   });
