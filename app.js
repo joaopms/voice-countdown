e827a545d59946040c1b29a4b76b7bbc79960e7a
João Pedro <joni_silvaco@hotmail.com>
2014-08-28 21:17:11 +0100

    Started to work on the static mode

--- app.js
+++ app.js
@@ -6,15 +6,34 @@ prompt.start();
 
 prompt.get([{
       name: "mode",
-      description: "What mode do you want to use? Static (1) or dynamic (2)?".yellow,
+      description: "What mode do you want to use? Static (1) or dynamic (2)?",
       message: "This is a required field and you should only use numbers (1 & 2) here!",
       type: "number",
       required: true,
       conform: function(mode) {
-        if (mode < 1 || mode > 2)
-          return false;
-        return true;
+        return mode >= 1 && mode <= 2;
       }}],
 function (err, results) {
-  console.log(results);
+  if (results.mode == 1) {
+    console.log("Static mode activated!");
+    staticMode();
+  } else {
+    console.log("Dynamic mode activated!");
+    dynamicMode();
+  }
 });
+
+function staticMode() {
+  prompt.get([{
+        name: "total",
+        description: "Total?",
+        message: "This is a required field and you should use a number greater than 0!",
+        type: "number",
+        required: true,
+        conform: function(total) {
+          return total >= 1;
+        }}],
+  function (err, results) {
+    console.log(results);
+  });
+}
