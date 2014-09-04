ecaabe0dc15f52b58cd1f80764587f2ba4c80e98
João Pedro <joni_silvaco@hotmail.com>
2014-09-04 23:46:18 +0100

    Added a method to prompt the user more easily

--- src/net/joaopms/voicecountdown/Main.java
+++ src/net/joaopms/voicecountdown/Main.java
@@ -7,11 +7,21 @@ import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException, JavaLayerException {
-        System.out.println("Hello world!");
+    public static void main(String[] args) {
+        System.out.println(promptUser("This is a test."));
+    }
 
+    public static String promptUser(String question) {
+        System.out.print(question + " ");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    // TODO Finish this and remove the exceptions from the method signature
+    public static void playAlert() throws JavaLayerException, FileNotFoundException {
         File file = new File("D:\\Downloads\\translate_tts.mp3");
         FileInputStream fileInputStream = new FileInputStream(file);
         BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
