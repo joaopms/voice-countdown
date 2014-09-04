44625ddaed36365c4f7068aa282e612b282d62ac
João Pedro <joni_silvaco@hotmail.com>
2014-09-04 04:37:33 +0100

    Testing the MP3 decoder (thanks JavaZOOM for working!)

--- src/net/joaopms/voicecountdown/Main.java
+++ src/net/joaopms/voicecountdown/Main.java
@@ -1,7 +1,20 @@
 package net.joaopms.voicecountdown;
 
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.Player;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, JavaLayerException {
         System.out.println("Hello world!");
+
+        File file = new File("D:\\Downloads\\translate_tts.mp3");
+        FileInputStream fileInputStream = new FileInputStream(file);
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
+        new Player(bufferedInputStream).play();
     }
 }
